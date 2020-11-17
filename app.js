2fff10630dd68b22a0b0374d0bfc99eaf558975c
Pei <peichuan@buffalo.edu>
2020-11-17 12:29:37 -0500

    'update'

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/dxp8yvyxy/", 
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
