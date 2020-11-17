4f63dbb43de0498149f5ccc0a47f064464a17fb5
Pei <peichuan@buffalo.edu>
2020-11-17 12:21:09 -0500

    'update'

--- app.js
+++ app.js
@@ -1,53 +1,316 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+
+const MongoDBStore = require("connect-mongo")(session);
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+
+
 const app = express();
-const request = require('request');
-const fetch = require('node-fetch');
-const bodyParser = require('body-parser');
-// name and image array [name:" ", image:"https://ddd" ]
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
-app.get('/',(req,res)=>{
-    res.render("landing");
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
 });
 
-app.get('/campgrounds', (req,res)=>{
-    let campgrounds = [
-        {name: "Salmon Creek", image:"https://www.photosforclass.com/download/px_1230302"},
-        {name: "Granite Hill", image:"https://www.photosforclass.com/download/px_2398220"},
-        {name: "Montain goat's rest", image:"https://www.photosforclass.com/download/px_712067"}
-    ]
-    res.render("campgrounds", {campgrounds: campgrounds});
-});
-
-app.post('/campgrounds', (req, res)=>{
-    res.send("You hit the post route")
-    // get data from form and add to campgrounds array
-    // redirect back to campgrounds page
-
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
 })
 
-app.get("/campgrounds/new", (req, res)=>{
-    res.render("new.ejs")
+const sessionConfig = {
+    store,
+    name: 'session',
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(helmet());
+
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=>{
-    console.log("The Yelp Server has started!");
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
 
 
 
 
-fetch('https://www.google.com/')
-.then((response)=>{
-    if (!response.ok){
-        throw new Error('Not ok');
-    }
-    // console.log(response);
-})
-.catch(error=>{
-    console.error('ERRaaaaOR', error)
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+// const request = require('request');
+// const fetch = require('node-fetch');
+// const bodyParser = require('body-parser');
+// // name and image array [name:" ", image:"https://ddd" ]
+
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.set("view engine", "ejs");
+
+
+// app.get('/',(req,res)=>{
+//     res.render("landing");
+// });
+
+// app.get('/campgrounds', (req,res)=>{
+//     let campgrounds = [
+//         {name: "Salmon Creek", image:"https://www.photosforclass.com/download/px_1230302"},
+//         {name: "Granite Hill", image:"https://www.photosforclass.com/download/px_2398220"},
+//         {name: "Montain goat's rest", image:"https://www.photosforclass.com/download/px_712067"}
+//     ]
+//     res.render("campgrounds", {campgrounds: campgrounds});
+// });
+
+// app.post('/campgrounds', (req, res)=>{
+//     res.send("You hit the post route")
+//     // get data from form and add to campgrounds array
+//     // redirect back to campgrounds page
+
+// })
+
+// app.get("/campgrounds/new", (req, res)=>{
+//     res.render("new.ejs")
+// })
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, ()=>{
+//     console.log("The Yelp Server has started!");
+// })
+
+
+
+
+// fetch('https://www.google.com/')
+// .then((response)=>{
+//     if (!response.ok){
+//         throw new Error('Not ok');
+//     }
+//     // console.log(response);
+// })
+// .catch(error=>{
+//     console.error('ERRaaaaOR', error)
+// })
